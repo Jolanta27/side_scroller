51b140b7e31f308623a9a6db79e2e9ccaf5b4c9a
Jolanta Galczynska <jolanta.galczynska@gmail.com>
2020-05-19 11:55:06 +0200

    Add navigation to the Scroller feature

--- Scroller.js
+++ Scroller.js
@@ -11,7 +11,7 @@ class Scroller {
 
         this.isThrottled = false;
 
-        this.isScrolledIntoView(this.sections[0])
+        this.drawNavigation();
     }
 
     isScrolledIntoView(el) {
@@ -49,9 +49,46 @@ class Scroller {
         this.scrollToCurrentSection();
     }
     scrollToCurrentSection = () => {
+        this.selectActiveNavItem();
         this.sections[this.currentSectionIndex].scrollIntoView({
             behavior: 'smooth',
             block: "start"
         })
+    }
+    drawNavigation = () => {
+        this.navigationContainer = document.createElement('aside');
+        this.navigationContainer.setAttribute('class', 'scroller__navigation');
+        const list = document.createElement('ul');
+
+        this.sections.forEach((section, index) => {
+            const listItem = document.createElement('li');
+
+            listItem.addEventListener('click', () => {
+                this.currentSectionIndex = index;
+
+                this.scrollToCurrentSection();
+            })
+
+            list.appendChild(listItem);
+        })
+        this.navigationContainer.appendChild(list);
+
+        document.body.appendChild(this.navigationContainer);
+
+        this.selectActiveNavItem();
+    }
+    selectActiveNavItem = () => {
+        if (this.navigationContainer) {
+            const navigationItem = this.navigationContainer.querySelectorAll('li');
+
+            navigationItem.forEach((item, index) => {
+                if (index === this.currentSectionIndex) {
+                    item.classList.add('active');
+                } else {
+                    item.classList.remove('active');
+                }
+            })
+        }
+
     }
 }
